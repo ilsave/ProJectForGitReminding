8fabb6070a55f6c6a89b78327819ae8e79efd6ea
ilsave <bulldoo@yandex.ru>
2020-08-27 09:57:18 +0300

    new line for commit to master

--- app/src/main/java/com/example/projectforgitreminding/MainActivity.kt
+++ app/src/main/java/com/example/projectforgitreminding/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
    print("Im here!")
+        print("I wanna add this line by merging to master branch!")
     }
 }
